4aa9a3f2901e811c9e2a724cfbe1889ad6c28f93
Tyler Hancock <tyler.hancock13@hotmail.com>
2020-04-03 22:19:08 -0300

    Section 8.1 Complete

---
+++ Section 8/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.android.marsrealestate.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+
+class OverviewViewModel : ViewModel() {
+
+
+    private val _response = MutableLiveData<String>()
+
+
+    val response: LiveData<String>
+        get() = _response
+
+
+    private var viewModelJob = Job()
+
+
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
+
+
+    init {
+        getMarsRealEstateProperties()
+    }
+
+
+    private fun getMarsRealEstateProperties() {
+        coroutineScope.launch {
+            // Get the Deferred object for our Retrofit request
+            var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
+            try {
+                // Await the completion of our Retrofit request
+                var listResult = getPropertiesDeferred.await()
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
